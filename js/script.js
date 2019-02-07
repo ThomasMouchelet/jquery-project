14e95501b8fb1e905dcfd14194cab4317d15de0b
thomasM <mouchelet.thomas@gmail.com>
2019-02-07 14:44:08 +0100

    add scroll top

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(window).on("scroll",function(){
+    let positionY = $(this).scrollTop();
+    console.log(positionY);
+    // $(window).height() //Hauteur de la page
+    
+    // Afficher arrototop une fois en bas de la page
+})
