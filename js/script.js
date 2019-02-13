fdbacb792e63cdb1c1224a81466b628e037c6032
thomasM <mouchelet.thomas@gmail.com>
2019-02-13 09:59:13 +0100

    add anim nav-bar

--- js/script.js
+++ js/script.js
@@ -8,8 +8,14 @@ $(window).on("scroll",function(){
     if(positionY < 1500){
         $("#scrolltotop").css("opacity", 0);
     }
-
     // lorsque positionY est supérieur à 100
+    if(positionY > 100){
         // réduire height de la nav 
-        // positionner la nav en fixe en haut de la page
+        $(".nav-bar").addClass("sup100");
+    }
+    if(positionY < 100){
+        $(".nav-bar").removeClass("sup100");
+    }
+    // positionner la nav en fixe en haut de la page
 })
+
